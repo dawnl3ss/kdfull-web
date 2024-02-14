7de8d047fecc388147759fa4764987a1921419c8
4m4Sec <neptune.dev.cybsec@gmail.com>
2024-02-14 20:42:54 +0100

    update [0.1.0] : home page started

--- view/home.view.php
+++ view/home.view.php
@@ -1,10 +1,37 @@
-<html>
+<!DOCTYPE html>
+<html lang="fr">
     <head>
-        <meta charset="utf-8">
-        <title> KDFull :: Home </title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../assets/css/style.css">
+        <title> KDFull :: Accueil </title>
     </head>
 
     <body>
-        <p> sale fdp d'enculé </p>
+        <nav class="navbar">
+            <img class="menu-hamburger" src="../assets/img/menu-hamburger.png">
+            <div class="nav-links">
+                <ul>
+                    <li><a href="/" class="active"> Accueil </a></li>
+                    <li><a href="/tutoriels"> Tutoriels </a></li>
+                    <li><a href="/reseaux"> Réseaux </a></li>
+                    <li><a href="/posts"> Publications </a></li>
+                </ul>
+            </div>
+            <a href="/" class="logo"> KDFull </a>
+        </nav>
+
+        <header>
+
+        </header>
+
+        <script>
+            const menu = document.querySelector(".menu-hamburger");
+            const nav = document.querySelector(".nav-links");
+
+            menu.addEventListener('click', () => {
+                nav.classList.toggle('mobile-menu');
+            });
+        </script>
     </body>
 </html>
