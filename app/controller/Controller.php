6a304756ee2ed43a3bdaa59472ce784b31f45a9a
dawnless <52249849+dawnl3ss@users.noreply.github.com>
2024-02-15 11:23:09 +0000

    update [0.4.0] : add home page details

--- app/controller/Controller.php
+++ app/controller/Controller.php
@@ -6,17 +6,20 @@ __load_all_classes();
 class AppController {
 
     public static function __views(){
-        create_routes(
-            [
-                [
-                    "method" => METHOD_GET,
-                    "name" => "/",
-                    "callback" => function(){
-                        require_once "view/home.view.php";
-                    }
-                ]
-            ]
-        );
+        
+        /**
+         * @route => /
+         */
+        create_route(METHOD_GET, '/', function (){
+            require_once "view/home.view.php";
+        });
+
+        /**
+         * @route => /tutoriels
+         */
+        create_route(METHOD_GET, '/tutoriels', function (){
+            echo "page";
+        });
         
         return load_routes();
     }
--- view/home.view.php
+++ view/home.view.php
@@ -2,26 +2,39 @@
 <html lang="fr">
     <head>
         <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="../assets/css/style.css">
         <title> KDFull :: Accueil </title>
+
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../vendors/fa/css/all.css">
+        <link rel="stylesheet" href="../assets/css/style.css">
     </head>
 
     <body>
-        <nav class="navbar">
-            <img class="menu-hamburger" src="../assets/img/menu-hamburger.png">
-            <div class="nav-links">
-                <ul>
-                    <li><a href="/" class="active"> Accueil </a></li>
-                    <li><a href="/tutoriels"> Tutoriels </a></li>
-                    <li><a href="/reseaux"> Réseaux </a></li>
-                    <li><a href="/posts"> Publications </a></li>
-                </ul>
-            </div>
-            <a href="/" class="logo"> KDFull </a>
-        </nav>
+        <header class="header">
+            <nav class="navbar">
+                <img class="menu-hamburger" src="../assets/img/menu-hamburger.png">
+                <div class="nav-links">
+                    <ul>
+                        <li><a href="/" class="active"> Accueil </a></li>
+                        <li><a href="/tutoriels"> Tutoriels </a></li>
+                        <li><a href="/reseaux"> Réseaux </a></li>
+                        <li><a href="/posts"> Publications </a></li>
+                    </ul>
+                </div>
+                <a href="/" class="logo"> KDFull </a>
+            </nav>
 
-        <header>
+            <div class="main-text">
+                <h1 class="main-title"> SOS Fibre Optique </h1>
+                <p class="main-p"> KD Full </p>
+            </div>
+
+            <div class="main-networks">
+                <a class="net-link" target="_blank" href="https://www.youtube.com/@sosfibreoptique"><i class="fa-brands fa-youtube fa-3x net-icon"></i></a>
+                <a class="net-link" target="_blank" href="https://www.facebook.com/solutioninformatiquecourtenay"><i class="fa-brands fa-facebook fa-3x net-icon"></i></a>
+                <a class="net-link" target="_blank" href="https://www.instagram.com/kdfull_officiel/"><i class="fa-brands fa-instagram fa-3x net-icon"></i></a>
+                <a class="net-link" target="_blank" href="https://www.tiktok.com/@kdfull?lang=fr"><i class="fa-brands fa-tiktok fa-3x net-icon"></i></a>
+            </div>
         </header>
 
         <script type="text/javascript" src="./../assets/js/app.js"></script>
